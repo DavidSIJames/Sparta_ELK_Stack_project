74b7a894a921629d463313799fde24c70c95b583
Tech-A46 <tech-a46@tech-a46s-air.home>
2018-11-16 21:57:04 +0000

    created kibana vagrant. however its not connecting to the elasticsearch instance

---
+++ kibana_provision.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+echo "==============================================================================="
+echo "===============              TEAM CANADIA: Kibana              ================"
+echo "==============================================================================="
+echo "_______________________________________________________________________________"
+echo "MMMMMMMMMMMMMMMMM                                             MMMMMMMMMMMMMMMMM"
+echo "MMMMMMMMMMMMMMMMM                      .                      MMMMMMMMMMMMMMMMM"
+echo "MMMMMMMMMMMMMMMMM                     .M.                     MMMMMMMMMMMMMMMMM"
+echo "MMMMMMMMMMMMMMMMM              .     .MMM.     .              MMMMMMMMMMMMMMMMM"
+echo "MMMMMMMMMMMMMMMMM              Mm,  .MMMMM.  ,mM              MMMMMMMMMMMMMMMMM"
+echo "MMMMMMMMMMMMMMMMM              qMMm,MMMMMMM,mMMq              MMMMMMMMMMMMMMMMM"
+echo "MMMMMMMMMMMMMMMMM         .    'MMMMMMMMMMMMMMM'    .         MMMMMMMMMMMMMMMMM"
+echo "MMMMMMMMMMMMMMMMM        .Mm.   MMMMMMMMMMMMMMM   .mM.        MMMMMMMMMMMMMMMMM"
+echo "MMMMMMMMMMMMMMMMM 'MMMMMMMMMMM. 'MMMMMMMMMMMMM' .MMMMMMMMMMM' MMMMMMMMMMMMMMMMM"
+echo "MMMMMMMMMMMMMMMMM  'MMMMMMMMMMMM.MMMMMMMMMMMMM.MMMMMMMMMMMM'  MMMMMMMMMMMMMMMMM"
+echo "MMMMMMMMMMMMMMMMM   'MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM'   MMMMMMMMMMMMMMMMM"
+echo "MMMMMMMMMMMMMMMMM .mMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMm. MMMMMMMMMMMMMMMMM"
+echo "MMMMMMMMMMMMMMMMM    ''MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM''    MMMMMMMMMMMMMMMMM"
+echo "MMMMMMMMMMMMMMMMM        ''MMMMMMMMMMMMMMMMMMMMMMMMM''        MMMMMMMMMMMMMMMMM"
+echo "MMMMMMMMMMMMMMMMM           ''MMMMMMMMMMMMMMMMMMM''           MMMMMMMMMMMMMMMMM"
+echo "MMMMMMMMMMMMMMMMM           mMMMMMMMMMMMMMMMMMMMMMm           MMMMMMMMMMMMMMMMM"
+echo "MMMMMMMMMMMMMMMMM                     MMM                     MMMMMMMMMMMMMMMMM"
+echo "MMMMMMMMMMMMMMMMM                     MMM                     MMMMMMMMMMMMMMMMM"
+echo "MMMMMMMMMMMMMMMMM                     MMM                     MMMMMMMMMMMMMMMMM"
+echo "MMMMMMMMMMMMMMMMM_____________________________________________MMMMMMMMMMMMMMMMM"
+echo " "
+echo "=========== Inital update and upgrade ==========="
+echo " "
+sudo apt-get update
+sudo apt-get upgrade -y
+echo " "
+echo "================= Installing Kibana ================="
+echo " "
+wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
+echo "deb https://artifacts.elastic.co/packages/6.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-6.x.list
+sudo apt-get update
+sudo apt-get install kibana -y
+echo " "
+echo "================= Configuring Kibana ================="
+echo " "
+sudo mv /etc/kibana/kibana.yml /etc/kibana/kibana.yml.old
+sudo cp /home/vagrant/kibana_templates/kibana.yml /etc/kibana/kibana.yml
+echo " "
+echo "================= Starting Kibana ================="
+echo " "
+sudo systemctl enable kibana
+sudo systemctl start kibana
+echo " "
+echo "================= Installing nginx ================="
+echo " "
+sudo apt-get install nginx -y
+echo " "
+echo "================= Starting nginx ================="
+echo " "
+sudo systemctl enable nginx
+sudo systemctl start nginx
+echo " "
+echo "================= Configuring nginx ================="
+echo " "
+sudo mv /etc/nginx/sites-available/default /etc/nginx/sites-available/default.ol
+sudo ln -s /home/vagrant/kibana_templates/proxy.conf /etc/nginx/sites-available/proxy.conf
+sudo service nginx restart
+echo " "
+echo "================= Provision Complete ================="
+echo " "
